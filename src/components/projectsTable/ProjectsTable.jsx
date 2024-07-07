95d957b9d05367186ad98e31552244a1c5d44a60
unknown <engr.adnanhossain@gmail.com>
2024-07-07 18:07:32 +0600

    complete nav tab active and archive

--- src/components/projectsTable/ProjectsTable.jsx
+++ src/components/projectsTable/ProjectsTable.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { IoArchiveOutline } from 'react-icons/io5';
+import { HiOutlineArchiveBoxArrowDown, HiOutlineArchiveBoxXMark } from 'react-icons/hi2';
 
-const ProjectsTable = () => {
+const ProjectsTable = ({ archive }) => {
     return (
         <div className='bg-white mt-5 rounded-md'>
             <div className='p-4 border-b last:border-none flex items-center justify-between hover:cursor-pointer'>
@@ -9,21 +9,21 @@ const ProjectsTable = () => {
                     <p className='h-2 w-2 bg-red-600 rounded-full'></p>
                     <h1 className='uppercase'>New Project</h1>
                 </div>
-                <IoArchiveOutline />
+                {archive === 'archive' ? <HiOutlineArchiveBoxXMark /> : <HiOutlineArchiveBoxArrowDown />}
             </div>
             <div className='p-4 border-b last:border-none flex items-center justify-between hover:cursor-pointer'>
                 <div className='flex items-center gap-x-2'>
                     <p className='h-2 w-2 bg-green-600 rounded-full'></p>
                     <h1 className='uppercase'>New Project</h1>
                 </div>
-                <IoArchiveOutline />
+                {archive === 'archive' ? <HiOutlineArchiveBoxXMark /> : <HiOutlineArchiveBoxArrowDown />}
             </div>
             <div className='p-4 border-b last:border-none flex items-center justify-between hover:cursor-pointer'>
                 <div className='flex items-center gap-x-2'>
                     <p className='h-2 w-2 bg-blue-600 rounded-full'></p>
                     <h1 className='uppercase'>New Project</h1>
                 </div>
-                <IoArchiveOutline />
+                {archive === 'archive' ? <HiOutlineArchiveBoxXMark /> : <HiOutlineArchiveBoxArrowDown />}
             </div>
         </div>
     );
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { IoArchiveOutline, IoFolderOutline } from 'react-icons/io5';
+import React, { useState } from 'react';
+import { IoFolderOutline } from 'react-icons/io5';
 import ProjectsTable from '../../components/projectsTable/ProjectsTable';
+import { HiOutlineArchiveBoxArrowDown } from 'react-icons/hi2';
 
 const ProjectsPage = () => {
+    const [archive, setArchive] = useState('active')
     return (
         <div>
             <div className='flex items-center justify-between'>
                 <h1 className='text-2xl font-medium'>Projects</h1>
                 <div className='flex items-center gap-x-3'>
-                    <button className='flex items-center gap-1 text-sm bg-gray-200 px-2 py-1.5 rounded-md'><IoFolderOutline /> Active</button>
-                    <button className='flex items-center gap-1 text-sm bg-gray-200 px-2 py-1.5 rounded-md'><IoArchiveOutline /> Archived</button>
+                    <button onClick={() => setArchive('active')} className={`flex items-center gap-1 text-sm px-2 py-1.5 rounded-md ${archive === 'active' ? 'bg-gray-200' : ''}`}><IoFolderOutline /> Active</button>
+                    <button onClick={() => setArchive('archive')} className={`flex items-center gap-1 text-sm px-2 py-1.5 rounded-md ${archive === 'archive' ? 'bg-gray-200' : ''}`}><HiOutlineArchiveBoxArrowDown /> Archived</button>
                     <button className='bg-blue-600 px-2 py-1.5 text-sm text-white rounded-md'>Create Project</button>
                 </div>
             </div>
-            <ProjectsTable />
+            <ProjectsTable archive={archive} />
         </div>
     );
 };
